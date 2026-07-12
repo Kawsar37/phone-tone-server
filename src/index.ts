7b90072a6c8d423d43fce0acb882e0a14ef8339c
Kawsar37 <kawsarali750@gmail.com>
2026-07-12 21:31:05 +0600

    index.ts cors policies update

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL?.split(",") || "http://localhost:3000",
+    origin: (process.env.CLIENT_URL as string) || "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "x-vercel-*"],
